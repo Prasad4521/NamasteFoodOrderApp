3ba872366c5396e258232341f20e944a9f05afef
Prashanth Goud <145241730+prashanth2612@users.noreply.github.com>
2024-06-08 09:21:46 +0530

    first commit

---
+++ dist/index.c36f364e.js
@@ -0,0 +1,11 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+const heading = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, React.createElement("h1", {}, "Namaste React")));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.c36f364e.js.map
